e6a821cd95cc58292ff0728cfd6876708f444fe9
kyleocit <1569241+kyleocit@users.noreply.github.com>
2025-01-05 13:15:33 -0500

    added PrimeVue for ui components

--- frontend/src/layouts/AuthenticatedLayout.vue
+++ frontend/src/layouts/AuthenticatedLayout.vue
@@ -1,7 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const toggleDarkMode = () => {
+  document.documentElement.classList.toggle('dark')
+}
+</script>
 
 <template>
-  <router-view />
+  <Button label="Toggle Dark Mode" @click="toggleDarkMode()" />
+  <main class="bg-surface-0 dark:bg-surface-950">
+    <router-view />
+  </main>
 </template>
 
 <style scoped></style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,23 @@
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import DefaultTheme from '@/themes/default'
 import router from '@/router'
 import '@/style.css'
 import App from '@/App.vue'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .use(PrimeVue, {
+    theme: {
+      preset: DefaultTheme,
+      options: {
+        prefix: 'p',
+        darkModeSelector: '.dark',
+        cssLayer: {
+          name: 'primevue',
+          order: 'tailwind-base, primevue, tailwind-utilities',
+        },
+      },
+    },
+  })
+  .mount('#app')
