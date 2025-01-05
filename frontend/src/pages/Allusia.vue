21107b3ecf3907760d97d04245c50e7b3b0b2906
BindonWrathbite <165421910+BindonWrathbite@users.noreply.github.com>
2025-01-05 16:02:15 -0500

    Updated Alusia.vue with world information, map, company logo

--- frontend/src/pages/Allusia.vue
+++ frontend/src/pages/Allusia.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <h1>Allusia</h1>
-    <p>Coming soon...</p>
+  <div class="bg-black h-45">
+    <img src="../assets/adv20-logo.jpg" alt="Advantage20 Logo" class="scale-50 origin-top-left" />
+  </div>
+  <div class="bg-black">
+    <h1 class="text-blue-200 text-7xl font-cinzel text-center">Allusia</h1>
+    <p class="text-white text-2xl text-center">
+      The modern world of Allusia was born from the original, singular continent of Talanari. After the 2nd sundering,
+      brought forth by the great crash and defeat of the God Drake: Drathgorog, Talanari began to rumble and spread
+      rapidly into 4 separate continents and a rumored set of islands. These continents and islands would be named by
+      the heartiest and most ancient of races: the dwarves and elves. The continents would soon be named: Sha’harragh,
+      Wildengraad, Elishkäva, Kor’amon and Drathgorog Isles. As time ticks on, Allusia would find itself inhabited by
+      numerous creatures, races, and natural structures such as forests, mountains, and many more. The intelligent races
+      would form cities, villages, towns, and various groups of like-minded individuals. The creatures would roam wild
+      in packs or as lone wolves, hoping to pick off smaller prey. With the recent developments of Allusia, there are
+      many experiences and adventures to be had. Are you willing to take up the reigns and find your place in this
+      world? If so, its future may lie in your hands. Happy Adventuring!
+    </p>
+    <img src="../assets/world-map.png" alt="Allusia Map" class="object-fill" />
   </div>
 </template>
-
 
 <style scoped></style>
